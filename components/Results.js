d3f2b6f4a1abee813be8b589e7a4adb5003fb5dd
Paul Gay <paulgay@Pauls-MacBook-Air.local>
2021-12-11 17:42:43 -0500

    removed flip-move

--- components/Results.js
+++ components/Results.js
@@ -1,14 +1,14 @@
 // import requests from "../utils/requests"
 import Thumbnail from "./Thumbnail"
-import FlipMove from "react-flip-move"
+// import FlipMove from "react-flip-move"
 
 const Results = ({ results }) => {
     return (
-        <FlipMove className="px-5 my-10 sm:grid md:grid-cols-2 xl:grid-cols-3 3xl:flex flex-wrap justify-start">
+        <div className="px-5 my-10 sm:grid md:grid-cols-2 xl:grid-cols-3 3xl:flex flex-wrap justify-start">
             {results.map((result) => (
                 <Thumbnail key={result.id} result={result} />
             ))}
-        </FlipMove>
+        </div>
     )
 }
 
